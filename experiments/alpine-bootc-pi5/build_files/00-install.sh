188073ad32e39959e2585ce8c197dfe4fae5d80d
bupd <bupdprasanth@gmail.com>
2026-07-15 02:30:28 +0530

    make pi image lean by default

--- experiments/alpine-bootc-pi5/build_files/00-install.sh
+++ experiments/alpine-bootc-pi5/build_files/00-install.sh
@@ -4,29 +4,18 @@ set -eux
 cat > /etc/apk/repositories <<'EOF'
 https://dl-cdn.alpinelinux.org/alpine/v3.23/main
 https://dl-cdn.alpinelinux.org/alpine/v3.23/community
-@edge-testing https://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
 apk update
 apk add --no-cache \
     alpine-base \
     bash \
-    bluez \
-    bluez-openrc \
-    buildah \
     busybox-mdev-openrc \
-    chrony \
-    cloudflared@edge-testing \
-    cni-plugins \
     curl \
     dosfstools \
-    dracut \
     e2fsprogs \
     fuse-overlayfs \
-    ip6tables \
     iptables \
-    iwd \
-    k3s \
     libarchive \
     libgcc \
     linux-firmware-brcm \
@@ -37,21 +26,13 @@ apk add --no-cache \
     openssl \
     ostree \
     podman \
-    podman-compose \
     raspberrypi-bootloader \
-    raspberrypi-utils \
-    restic \
     skopeo \
     slirp4netns \
     sudo \
-    tailscale \
-    tmux \
-    ufw \
     util-linux \
     util-linux-misc \
-    wget \
     wireless-regdb \
-    wireguard-tools \
     wpa_supplicant
 
 adduser -D -s /bin/ash bupd
