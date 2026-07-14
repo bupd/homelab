f73ba25728a4f5812f87f15ffaf27f848d595b67
Prasanth Baskar <bupdprasanth@gmail.com>
2026-07-14 15:47:54 +0530

    add alpine pi base install scripts (#2)

---
+++ experiments/alpine-bootc-pi5/build_files/00-install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+set -eux
+
+cat > /etc/apk/repositories <<'EOF'
+https://dl-cdn.alpinelinux.org/alpine/v3.23/main
+https://dl-cdn.alpinelinux.org/alpine/v3.23/community
+@edge-testing https://dl-cdn.alpinelinux.org/alpine/edge/testing
+EOF
+
+apk update
+apk add --no-cache \
+    alpine-base \
+    bash \
+    bluez \
+    bluez-openrc \
+    buildah \
+    busybox-mdev-openrc \
+    chrony \
+    cloudflared@edge-testing \
+    cni-plugins \
+    curl \
+    dosfstools \
+    dracut \
+    e2fsprogs \
+    fuse-overlayfs \
+    ip6tables \
+    iptables \
+    iwd \
+    k3s \
+    linux-firmware-brcm \
+    linux-rpi \
+    netavark \
+    openssh \
+    openssl \
+    ostree \
+    podman \
+    podman-compose \
+    raspberrypi-bootloader \
+    raspberrypi-utils \
+    restic \
+    skopeo \
+    slirp4netns \
+    sudo \
+    tailscale \
+    tmux \
+    ufw \
+    wget \
+    wireless-regdb \
+    wireguard-tools \
+    wpa_supplicant
+
+adduser -D -s /bin/ash bupd
+addgroup bupd wheel
+echo '%wheel ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
+chmod 0440 /etc/sudoers.d/wheel
+
+echo node01 > /etc/hostname
+echo uninitialized > /etc/machine-id
+
+ssh-keygen -A
---
+++ experiments/alpine-bootc-pi5/build_files/10-bootc-rootfs.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -eux
+
+mkdir -p /usr/lib/rpi-boot
+cp -a /boot/. /usr/lib/rpi-boot/
+
+kernel="$(find /lib/modules -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | sort -V | tail -n 1)"
+mkinitfs -o /usr/lib/rpi-boot/initramfs-rpi "$kernel"
+
+rm -rf /boot /home /root /srv /opt /mnt /var/cache/apk/*
+
+mkdir -p /sysroot /boot /usr/lib/ostree /var /var/home/bupd /var/roothome /var/srv /var/opt /var/mnt
+chown 1000:1000 /var/home/bupd
+
+ln -sT sysroot/ostree /ostree
+ln -sT var/home /home
+ln -sT var/roothome /root
+ln -sT var/srv /srv
+ln -sT var/opt /opt
+ln -sT var/mnt /mnt
+
+cat > /usr/lib/ostree/prepare-root.conf <<'EOF'
+[composefs]
+enabled = yes
+
+[sysroot]
+readonly = true
+EOF
+
+mkdir -p /usr/lib/tmpfiles.d
+cat > /usr/lib/tmpfiles.d/alpine-bootc-base.conf <<'EOF'
+d /var/home 0755 root root -
+d /var/roothome 0700 root root -
+d /var/srv 0755 root root -
+d /var/opt 0755 root root -
+d /var/mnt 0755 root root -
+d /var/lib/containers 0711 root root -
+d /run/media 0755 root root -
+EOF
+
+cat > /etc/fstab <<'EOF'
+tmpfs /tmp tmpfs nosuid,nodev 0 0
+EOF
+
+cat > /etc/motd <<'EOF'
+Experimental Alpine bootc Raspberry Pi 5 node.
+EOF
